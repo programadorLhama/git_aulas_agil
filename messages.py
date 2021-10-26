3aea9503d98874713e1cce203d204096b451d75c
programadorLhama <programadorLhama@gmail.com>
2021-10-25 23:04:29 -0300

    feat: Adding new messages to project

--- messages.py
+++ messages.py
@@ -1,5 +1,6 @@
 display_messages = [
     'Seja Feliz :)',
     'Fique tranquilo, tudo vai acabar bem!',
-    'ola mundo! estou aqui!'
+    'ola mundo! estou aqui!',
+    'Acredite nos seus caminhos! Sempre!'
 ]
