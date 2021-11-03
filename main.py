983653955fa178d698ed92d465c911989ebe9e5b
programadorLhama <programadorLhama@gmail.com>
2021-11-03 08:25:23 -0300

    feat: Implementing Presentation before project start

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from messages import display_messages
 import random
+import time
 
-print('Starting project again')
+print('Iniciando o Projeto')
+time.sleep(3)
 
 while True:
     resposta = input('Deseja Receber um Conselho? S/N: ')
