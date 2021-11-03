e9f80a1855a2564029128d2a9dc2ed6c83d80ca2
programadorLhama <programadorLhama@gmail.com>
2021-11-03 08:26:09 -0300

    feat: Implementing light message

--- messages.py
+++ messages.py
@@ -2,5 +2,6 @@ display_messages = [
     'Seja Feliz :)',
     'Fique tranquilo, tudo vai acabar bem!',
     'ola mundo! estou aqui!',
-    'Acredite nos seus caminhos! Sempre!'
+    'Acredite nos seus caminhos! Sempre!',
+    'Sempre apague as luzes antes de sair!'
 ]
