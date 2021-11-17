5c0baa9ca202681fd0eeda66289c9703aa9dd5da
programadorLhama <programadorLhama@gmail.com>
2021-11-16 22:34:19 -0300

    feat: Implementing login

--- main.py
+++ main.py
@@ -5,9 +5,23 @@ import time
 print('Iniciando o Projeto 3..2..1..')
 time.sleep(3)
 
+login_username = [
+    'Rafael',
+    'Luiz',
+    'Joazinho'
+]
+
+username = input('usuario para login: ')
+
 while True:
+    while(username not in login_username):
+        print()
+        print('Usuario Invalido! Tente novamente! ')
+        username = input('usuario para login: ')
+
     resposta = input('Deseja Receber um Conselho? S/N: ')
     if (resposta == 'S' or resposta == 's'):
+        print()
         mensagem = random.choice(display_messages)
         print(mensagem)
         print()
